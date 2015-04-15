1bfeae68fac008ed0498588e4ae7f3fd8b16f48a
hectorsim <hector90@gmail.com>
2015-04-16 04:53:18 +0800

    IS421 Java enabled OPL

---
+++ IS421/src/util/Constants.java
@@ -0,0 +1,21 @@
+package util;
+
+public class Constants {
+	
+	/*
+	 * Settings for .dat file generation
+	 */
+	public static final int dashValue = 99999;
+	public static final int satisfactionDecreaseStep = 10;
+	public static final String PRICEMATRIXCSV = "/data/price_matrix.csv";
+	
+	public static final String DATADIR = "itineraryplanner/";
+	public static final String MODSTRING = "warehouse.mod";
+	public static final String DATSTRING = "travel.dat";
+
+	/*
+	 * Debug mode
+	 */
+	public static final boolean debug = false;
+	
+}
